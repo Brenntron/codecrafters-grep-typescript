394719fd4b40354e3a44e0db86796ed5072cdf60
Brennan (Brenntron) Willingham <brennan.willingham@gmail.com>
2025-10-09 12:38:55 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,28 +6,28 @@ const inputLine: string = await Bun.stdin.text();
 function matchPattern(inputLine: string, pattern: string): boolean {
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
+  } else if (pattern.startsWith('[^') && pattern.endsWith(']')) {
+    const charsToMatch = pattern.slice(2, pattern.length - 1).split('')
+    return inputLine.split('').some((char: string): boolean  => !charsToMatch.includes(char));
   } else if (pattern.startsWith('[') && pattern.endsWith(']')) {
-    const substringToMatch = pattern.slice(1, pattern.length - 1)
-
-    return inputLine.split('').some((char) => substringToMatch.includes(char));
+    const charsToMatch = pattern.slice(1, pattern.length - 1).split('')
+    return inputLine.split('').some((char: string): boolean  => charsToMatch.includes(char));
   }
 
   switch (pattern) {
     case '\\d':
-      return inputLine.split('').some((char) => char >= '0' && char <= '9');
+      return inputLine.split('').some((char: string): boolean => char >= '0' && char <= '9');
     case '\\w':
-      return inputLine.split('').some((char) => (char.toLowerCase() >= 'a' && char.toLowerCase() <= 'z') || (char >= '0' && char <= '9') || char === '_');
+      return inputLine.split('').some((char: string): boolean => (char.toLowerCase() >= 'a' && char.toLowerCase() <= 'z') || (char >= '0' && char <= '9') || char === '_');
     default:
       throw new Error(`Unhandled pattern: ${pattern}`);
   }
 }
 
 if (args[2] !== "-E") {
-  console.log("Expected first argument to be '-E'");
   process.exit(1);
 }
 
-// Uncomment this block to pass the first stage
 if (matchPattern(inputLine, pattern)) {
   process.exit(0);
 } else {
